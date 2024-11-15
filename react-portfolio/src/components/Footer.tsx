3a32b3701a0ac686f4746cb133d6c0d534d2418c
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-11-15 15:35:03 -0500

    Added Footer component

---
+++ react-portfolio/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/acgianesi" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://linkedin.com/in/andre-gianesi" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+    </footer>
+  );
+};
+
+export default Footer;
