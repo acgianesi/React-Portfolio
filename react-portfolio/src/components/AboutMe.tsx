c1e8aab117bb5c3c26aa189c52e819dd52723050
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-11-15 15:01:18 -0500

    Added AboutMe component

---
+++ react-portfolio/src/components/AboutMe.tsx
@@ -0,0 +1,11 @@
+const AboutMe = () => {
+  return (
+    <section id="aboutMe">
+      <h2>About Me</h2>
+      <img src="/public/profilepic.jpeg" alt="Andre's photo" style={{ width: '150px', borderRadius: '50%' }} />
+      <p>Hi, I'm Andre! I'm a web developer passionate about building interactive and user-friendly web applications. I love learning new technologies and always strive to improve my coding skills.</p>
+    </section>
+  );
+};
+
+export default AboutMe;
