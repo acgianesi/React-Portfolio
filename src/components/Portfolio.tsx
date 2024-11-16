02ec00d058011f531abfe17c6307a1508a1d1c9b
André Company Gianesi <andrecompanygianesi@Andres-Air.lan>
2024-11-16 17:49:23 -0500

    trying to fix netilify img deployment

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -8,42 +8,42 @@ const Portfolio = () => {
       <div className="portfolio-container">
 
         <Project
-          imageUrl="/public/readmepic.webp"
+          imageUrl="/readmepic.webp"
           title="README Generator"
           description="Generates a README.md file for new projects."
           repoUrl="https://github.com/acgianesi/README-Generator"
         />
 
         <Project
-          imageUrl="/public/employeetrackerpic.png"
+          imageUrl="/employeetrackerpic.png"
           title="Employee Tracker"
           description="The Employee Tracker is a command-line application designed to manage a company's employee database using Node.js, Inquirer, and PostgreSQL."
           repoUrl="https://github.com/acgianesi/Employee-Tracker"
         />
 
         <Project
-          imageUrl="/public/vehiclebuilder.webp"
+          imageUrl="/vehiclebuilder.webp"
           title="Vehicle Builder"
           description="Vehicle-Builder is a command-line application that enables users to create or select vehicles—specifically cars, trucks, and motorbikes—and perform a variety of actions with them."
           repoUrl="https://github.com/acgianesi/Vehicle-Builder"
         />
 
         <Project
-          imageUrl="/public/reactapp.png"
+          imageUrl="/reactapp.png"
           title="Roundhouse Jokes"
           description="Roundhouse Jokes is your go-to app for an endless supply of laughs!"
           repoUrl="https://github.com/MCFoster007/ROUNDHOUSEJOKES"
         />
 
         <Project
-          imageUrl="/public/placeholder.png"
+          imageUrl="/placeholder.png"
           title="Project Five"
           description="A description of Project Five."
           repoUrl="https://github.com/acgianesi"
         />
 
         <Project
-          imageUrl="/public/placeholder.png"
+          imageUrl="/placeholder.png"
           title="Project Six"
           description="A description of Project Six."
           repoUrl="https://github.com/acgianesi"
