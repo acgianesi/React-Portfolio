e8ca2705147ad84ac5f8338a79882f04d2454f6d
André Company Gianesi <andrecompanygianesi@Andres-Air.lan>
2024-11-16 14:24:25 -0500

    Added Resume component

---
+++ react-portfolio/src/components/Resume.tsx
@@ -0,0 +1,18 @@
+const Resume = () => {
+    return (
+      <section id="resume">
+        <h2>Resume</h2>
+        <a href="/path/to/resume123.pdf" download>Download My Resume</a>
+        
+        <h3>Skills and Proficiencies</h3>
+        <ul>
+          <li>JavaScript</li>
+          <li>React.js</li>
+          <li>HTML/CSS</li>
+          <li>Node.js</li>
+        </ul>
+      </section>
+    );
+  };
+  
+  export default Resume;
