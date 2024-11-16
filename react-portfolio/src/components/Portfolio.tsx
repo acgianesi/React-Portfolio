67fe878686c1d309e24a4a87b42ef30cb1e5202d
André Company Gianesi <andrecompanygianesi@Andres-Air.lan>
2024-11-16 13:18:04 -0500

    Added Portfolio component (links, description, images)

---
+++ react-portfolio/src/components/Portfolio.tsx
@@ -0,0 +1,56 @@
+import Project from './Project';
+
+const Portfolio = () => {
+  return (
+    <section id="portfolio">
+      <h2>Portfolio</h2>
+
+      <div className="portfolio-container">
+
+        <Project
+          imageUrl="/public/readmepic.webp"
+          title="README Generator"
+          description="Generates a README.md file for new projects."
+          repoUrl="https://github.com/acgianesi/README-Generator"
+        />
+
+        <Project
+          imageUrl="/employeetrackerpic.png"
+          title="Employee Tracker"
+          description="The Employee Tracker is a command-line application designed to manage a company's employee database using Node.js, Inquirer, and PostgreSQL."
+          repoUrl="https://github.com/acgianesi/Employee-Tracker"
+        />
+
+        <Project
+          imageUrl="/vehiclebuilder.webp"
+          title="Vehicle Builder"
+          description="Vehicle-Builder is a command-line application that enables users to create or select vehicles—specifically cars, trucks, and motorbikes—and perform a variety of actions with them."
+          repoUrl="https://github.com/acgianesi/Vehicle-Builder"
+        />
+
+        <Project
+          imageUrl="/public/reactapp.png"
+          title="Roundhouse Jokes"
+          description="Roundhouse Jokes is your go-to app for an endless supply of laughs!"
+          repoUrl="https://github.com/MCFoster007/ROUNDHOUSEJOKES"
+        />
+
+        <Project
+          imageUrl="/public/placeholder.png"
+          title="Project Five"
+          description="A description of Project Five."
+          repoUrl="https://github.com/acgianesi"
+        />
+
+        <Project
+          imageUrl="/public/placeholder.png"
+          title="Project Six"
+          description="A description of Project Six."
+          repoUrl="https://github.com/acgianesi"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
